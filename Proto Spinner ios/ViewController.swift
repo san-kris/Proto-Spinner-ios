164bf8f4be73d9aef13dd8acc08cdbc00e8f80ee
Santosh Krishnamurthy <san.b350@gmail.com>
2023-02-21 10:34:08 -0600

    Prototype Spinner and Progress Bar activity indicator on IOS

--- Proto Spinner ios/ViewController.swift
+++ Proto Spinner ios/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loadingView: UIView! {
+        didSet{
+            loadingView.layer.cornerRadius = 10
+        }
+    }
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    var progress: Float? {
+        didSet{
+            if let progress{
+                progressView.progress = progress
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        activityIndicator.startAnimating()
+        loadingView.isHidden = false
     }
 
-
+    @IBAction func showSpinnerPressed(_ sender: UIButton) {
+        activityIndicator.startAnimating()
+        loadingView.isHidden = false
+        progressView.isHidden = false
+        if let progress{
+            self.progress = progress + 0.1
+        } else{
+            self.progress = 0.1
+        }
+    }
+    
+    @IBAction func hideSpinnerPressed(_ sender: UIButton) {
+        activityIndicator.stopAnimating()
+        loadingView.isHidden = true
+        progressView.isHidden = true
+        self.progress = 0.1
+    }
+    
 }
 
